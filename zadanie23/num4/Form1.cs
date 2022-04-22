118c12e6811c1f3a43883a7d33764a06b615fc0a
Ita09 <nikitaracek@gmail.com>
2022-04-22 09:05:08 +0300

    zadanie23

--- zadanie23/num4/Form1.cs
+++ zadanie23/num4/Form1.cs
@@ -15,6 +15,7 @@ namespace num4
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,94 +23,58 @@ namespace num4
 
             if (textBox1.Text.ToString() == "К")
             {
-                //вызываем класс, который создает кнопку
+                Button b = new Button();
+                b.MouseMove += B_MouseMove;
+                b.Text = "Кнопка";
+                this.Controls.Add(b);
+                b.Location = new Point(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                b.BackColor = Color.Pink;
+                b.Show();
+                
+                
             }
             else if (textBox1.Text.ToString() == "П")
             {
 
-                //вызываем класс, который создает поле ввода
+                TextBox t = new TextBox();
+                t.MouseMove += T_MouseMove;
+                t.Text = "Текстовое поле";
+                this.Controls.Add(t);
+                t.Location = new Point(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                t.BackColor = Color.Red;
+                t.Show();
             }
             else if (textBox1.Text.ToString() == "Л")
             {
-                //вызываем класс, который создает метку
+                Label l = new Label();
+                l.MouseMove += L_MouseMove;
+                l.Text = "Метка";
+                this.Controls.Add(l);
+                l.Location = new Point(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                l.BackColor = Color.Green;
+                l.Show();
             }
 
 
 
         }
-        public void InputBox()
+
+        private void B_MouseMove(object sender, MouseEventArgs e)
         {
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Size = new Size(300, 150);
-            this.Text = "Заголовок";
-
-            
-            textBox1.Size = new Size(250, 25);
-            textBox1.Font = new Font(TextBox.DefaultFont, FontStyle.Regular);
-            textBox1.Location = new Point(20, 50);
-
-            this.Controls.Add(textBox1);
-
-            textBox1.Show();
-
-            textBox1.KeyPress += new KeyPressEventHandler(textBox_KeyPress);
-
-            Label label = new Label();
-            label.AutoSize = false;
-            label.Size = new Size(250, 25);
-            label.Font = new Font(label.Font, FontStyle.Regular);
-            label.Location = new Point(20, 25);
-            label.Text = "Введите новое значение:  ";
-
-            this.Controls.Add(label);
-
-            label.Show();
-
-            Button buttonOK = new Button();
-            buttonOK.Size = new Size(80, 25);
-            buttonOK.Location = new Point(105, 75);
-            buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            buttonOK.Text = "OK";
-
-            this.Controls.Add(buttonOK);
-
-            buttonOK.Show();
-
-            Button buttonCancel = new Button();
-            buttonCancel.Size = new Size(80, 25);
-            buttonCancel.Location = new Point(190, 75);
-            buttonCancel.Text = "Cancel";
-
-            this.Controls.Add(buttonCancel);
-
-            buttonCancel.Show();
-
-            buttonCancel.Click += new EventHandler(buttonCancel_Click);
+            Button b = (Button)sender;
+            b.Dispose();
         }
 
-        public void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        private void T_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.KeyChar == (Char)Keys.Enter)
-            {
-                this.DialogResult = System.Windows.Forms.DialogResult.OK;
-
-                this.Close();
-            }
+            TextBox t = (TextBox)sender;
+            t.Dispose();
         }
 
-        public void buttonCancel_Click(object sander, EventArgs e)
+        private void L_MouseMove(object sender, MouseEventArgs e)
         {
-            this.Close();
-        }
-
-        public string getString()
-        {
-            if (this.ShowDialog() != System.Windows.Forms.DialogResult.OK)
-                return null;
-            return textBox1.Text;
+            Label l = (Label)sender;
+            l.Dispose();
         }
     }
 }
